3d614fbb9dcb8f5cded99f2c59f25065cce8fa9a
daeyoung <pstor23@gmail.com>
2024-12-23 10:02:57 +0900

    refactor: repository, imageservice 리팩토링
    *refactor: findById -> getById 사용
    
    *refactor: imageservice에서 Resource 얻기
    
    *feat: 브랜드 이미지 조회 추가

--- src/main/java/com/example/bestme/repository/BrandRepository.java
+++ src/main/java/com/example/bestme/repository/BrandRepository.java
@@ -5,8 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 public interface BrandRepository extends JpaRepository<Brand, Long> {
+
+    default Brand getById(Long id) {
+        return findById(id)
+                .orElseThrow(() -> new NoSuchElementException("브랜드 찾을 수 없음"));
+    }
 
     @Query(value = """
         WITH RECURSIVE cte (id) AS (
