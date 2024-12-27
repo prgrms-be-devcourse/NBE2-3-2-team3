1c952b128e46710173a1fdf1740a9ec6d4be6eeb
youngseo Kim <yseokim0212@gmail.com>
2024-12-27 17:40:56 +0900

    [front] 마이페이지

--- src/main/java/com/example/bestme/controller/user/UserViewController.java
+++ src/main/java/com/example/bestme/controller/user/UserViewController.java
@@ -3,16 +3,9 @@ package com.example.bestme.controller.user;
 import com.example.bestme.service.KakaoService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -51,8 +44,20 @@ public class UserViewController {
     }
 
     @GetMapping("/member/my_posting")
-    public String MyPosting() {
-        return "member/my_posting";
+    public String MyPosting(Model model) {
+        // DB에서 자기가 쓴 게시물 불러와서
+        // model.addAttribute로 넘겨줘야할듯
+        return "my_posting";
+    }
+
+    @GetMapping("/member/modify_profile")
+    public String modifyProfile() {
+        return "modify_profile";
+    }
+
+    @GetMapping("/member/delete_profile")
+    public String deleteProfile() {
+        return "delete_profile";
     }
 
 }
