9b955494e050e6b70ecc169ab3bc67858ad22c49
MadPumpkin3 <fpdjxpa37@gmail.com>
2024-12-30 01:56:03 +0900

    Feat : Community 모든 html 생성(board 수업 파일 사용), html에 사용될 css 및 이미지 추가

--- src/main/java/com/example/bestme/controller/community/CommunityViewController.java
+++ src/main/java/com/example/bestme/controller/community/CommunityViewController.java
@@ -10,12 +10,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class CommunityViewController {
 
     // 커뮤니티 메인 view
-    @RequestMapping( "/community/{page}" )
+    @RequestMapping( value = {"/community", "/community/{page}"} )
     public String communityMain(@PathVariable(required = false) String page, Model model, HttpSession session) {
         // 현재 페이지 초기화
         String currentPageNumber;
 
-        if (page == null) {
+        if (page == null || page.equals("0")) {
             // URL 파라미터가 없을 때, 세션 값 사용 또는 기본값 설정
             currentPageNumber = (String) session.getAttribute("currentPageNumber");
             if (currentPageNumber == null) {
@@ -32,34 +32,36 @@ public class CommunityViewController {
         // 모델에 현재 페이지 추가 (뷰에서 사용할 데이터)
         model.addAttribute("currentPageNumber", currentPageNumber);
 
-        return "communityMain";
+        System.out.println(currentPageNumber);
+
+        return "community/community_main";
     }
 
     // 게시물 디테일 view
     @RequestMapping( "/community/detail/{boardId}" )
     public String communityDetail(@PathVariable String boardId, Model model) {
         model.addAttribute("boardId", boardId);
-        return "communityDetail";
+        return "community/community_detail";
     }
 
     // 게시물 생성 view
     @RequestMapping( "/community/write" )
     public String communityWrite() {
-        return "communityWrite";
+        return "community/community_write";
     }
 
     // 게시물 수정 view
-    @RequestMapping( "/community/update/{boardId}" )
+    @RequestMapping( "/community/modify/{boardId}" )
     public String communityUpdate(@PathVariable String boardId, Model model) {
         model.addAttribute("boardId", boardId);
-        return "communityUpdate";
+        return "community/community_modify";
     }
 
     // 게시물 삭제 view
     @RequestMapping( "/community/delete/{boardId}" )
     public String communityDelete(@PathVariable String boardId, Model model) {
         model.addAttribute("boardId", boardId);
-        return "communityDelete";
+        return "community/community_delete";
     }
 
     // 현재 User가 작성한 게시물 목록 view
@@ -86,6 +88,6 @@ public class CommunityViewController {
         // 모델에 현재 페이지 추가 (뷰에서 사용할 데이터)
         model.addAttribute("userPageNumber", userPageNumber);
 
-        return "communityMyBoards";
+        return "my_posting";
     }
 }
