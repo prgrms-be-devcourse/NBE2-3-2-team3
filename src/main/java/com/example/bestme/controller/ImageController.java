9af072a2862656ca4f221cd88f071fbd76701a44
daeyoung <pstor23@gmail.com>
2025-01-03 22:18:59 +0900

    docs: 스웨거 설정

--- src/main/java/com/example/bestme/controller/ImageController.java
+++ src/main/java/com/example/bestme/controller/ImageController.java
@@ -1,6 +1,8 @@
 package com.example.bestme.controller;
 
 import com.example.bestme.service.LocalImageService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
@@ -15,11 +17,13 @@ import java.net.MalformedURLException;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/images")
+@Tag(name = "Image", description = "저장된 이미지를 화면에 출력하는 API")
 public class ImageController {
 
     private final LocalImageService imageService;
 
     @GetMapping("/items/{itemId}")
+    @Operation( summary = "아이템 이미지 출력", description = "upload/item-img 디렉토리의 이미지 출력" )
     public ResponseEntity<Resource> downloadItemImage(@PathVariable Long itemId) throws MalformedURLException {
         Resource response = imageService.getItemImageResource(itemId);
         return ResponseEntity.ok()
@@ -28,6 +32,7 @@ public class ImageController {
     }
 
     @GetMapping("/brands/{brandId}")
+    @Operation( summary = "브랜드 이미지 출력", description = "upload/brand-img 디렉토리의 이미지 출력" )
     public ResponseEntity<Resource> downloadBrandImage(@PathVariable Long brandId) throws MalformedURLException {
         Resource response = imageService.getBrandImageResource(brandId);
         return ResponseEntity.ok()
--- src/main/java/com/example/bestme/controller/ItemViewController.java
+++ src/main/java/com/example/bestme/controller/ItemViewController.java
@@ -1,5 +1,7 @@
 package com.example.bestme.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -7,14 +9,17 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/items")
+@Tag(name = "ItemView", description = "아이템 페이지를 렌더링하기 위한 컨트롤러")
 public class ItemViewController {
 
     @GetMapping
+    @Operation( summary = "모든 아이템 페이지", description = "모든 아이템 페이지" )
     public String getItems() {
         return "items";
     }
 
     @GetMapping("/recommend")
+    @Operation( summary = "추천 아이템 페이지", description = "추천 아이템 페이지" )
     public String getRecommendItems() {
         return "recommend-item";
     }
